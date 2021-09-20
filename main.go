9988ab1718d5db48b5b2d409dbd084867a671979
Filip <szymanski.fp@gmail.com>
2021-09-20 21:10:35 +0200

    logging banner on initialization

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func init(){
+	file, err := ioutil.ReadFile("banner.txt")
+	if err != nil{
+		fmt.Println("FAILED TO READ BANNER, PROGRAMM STARTED!")
+	} else{
+		fileText := string(file)
+		fmt.Println(fileText)
+	}
+}
 
 
 func main(){
 	fmt.Println("Hello World!")
+
 }
